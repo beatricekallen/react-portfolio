2ba21d1d83fa73ddb7b5e395b5af1bbee40aa701
beatricekallen <bkallen90@gmail.com>
2022-07-01 14:08:10 -0400

    adds styling to project component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,29 +1,38 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Project = ({ projects }) => {
   return (
-    <div className="row">
-      {projects.map((project) => (
-        <Card style={{ width: "18rem" }} className="card">
-          <Card.Img variant="top" src={project.image} className="card-image" />
-          <Card.Body className="card-body">
-            <Card.Title>{project.title}</Card.Title>
-            <Card.Text>{project.description}</Card.Text>
-            <Button variant="primary" href={project.github}>
-              Check it out on GitHub
-            </Button>
-            <Button
-              variant="primary"
-              href={project.deployed}
-              className="button"
-            >
-              Check out the deployed app
-            </Button>
-          </Card.Body>
-        </Card>
-      ))}
-    </div>
+    <Container>
+      <Row>
+        {projects.map((project) => (
+          <Col>
+            <Card style={{ width: "25rem" }} className="card">
+              <Card.Img
+                variant="top"
+                src={project.image}
+                className="card-image"
+              />
+              <Card.Body className="card-body">
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Text>{project.description}</Card.Text>
+                <Button href={project.github}>Check it out on GitHub</Button>
+                <Button
+                  variant="primary"
+                  href={project.deployed}
+                  className="button"
+                >
+                  Check out the deployed app
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
