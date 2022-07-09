2afd633f6e02a8ce4dc79737689a8a4487202c1c
beatricekallen <bkallen90@gmail.com>
2022-07-09 15:05:09 -0400

    updates portfolio project cards to include technologies used

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
       title: "Twitcher",
       image: twitcherImage,
       description:
-        "A members-only blog for bird lovers in the Triangle, NC, area.",
+        "A members-only blog for bird lovers in the Triangle, NC, area. Built with JavaScript, HTML, Materialize CSS, Node.js, Express, Sequelize, Nodemailer, Handlebars, and bcrypt.",
       deployed: "https://triangle-twitchers.herokuapp.com/",
       github: "https://github.com/beatricekallen/twitcher",
     },
@@ -45,7 +45,7 @@ function App() {
       title: "Beers and Cheers",
       image: beersAndCheersImage,
       description:
-        "Need an app to help you plan the perfect night out? Gotcha covered.",
+        "Need an app to help you plan the perfect night out? Gotcha covered. Search for breweries and events in your area. Built with JavaScript, HTML, Bulma CSS, GitHub, Unsplash, Open Brewery API, and Ticketmaster API.",
       deployed: "https://beatricekallen.github.io/beers-and-cheers/",
       github: "https://github.com/beatricekallen/beers-and-cheers",
     },
@@ -54,7 +54,7 @@ function App() {
       title: "Weather Dashboard",
       image: weatherDashboardImage,
       description:
-        "Find out the current weather and the 5-day forecast in any city.",
+        "Find out the current weather and the 5-day forecast in any city. Built with JavaScript, Bootstrap, HTML, and Open Weather API.",
       deployed: "https://beatricekallen.github.io/weather-dashboard/",
       github: "https://github.com/beatricekallen/weather-dashboard",
     },
@@ -63,7 +63,7 @@ function App() {
       title: "Tech Blog",
       image: techBlogImage,
       description:
-        "An application that allows users to view and post blogs. The app also includes comment, edit, and delete functionalities.",
+        "An application that allows users to view and post blogs. The app also includes comment, edit, and delete functionalities. Built with Node.js, Express, and Sequelize. Follows the MVC paradigm.",
       deployed: "https://tech-blog-bka.herokuapp.com/",
       github: "https://github.com/beatricekallen/tech-blog",
     },
@@ -72,7 +72,7 @@ function App() {
       title: "Password Generator",
       image: passwordGeneratorImage,
       description:
-        "An application that generates a random password based on user input about how long they'd like the password to be and what characters they'd like to include.",
+        "An application that generates a random password based on user input about how long they'd like the password to be and what characters they'd like to include. Built with JavaScript, HTML, and CSS.",
       deployed: "https://beatricekallen.github.io/password-generator/",
       github: "https://github.com/beatricekallen/password-generator",
     },
@@ -81,7 +81,7 @@ function App() {
       title: "Work Day Scheduler",
       image: workdaySchedulerImage,
       description:
-        "This daily planner allows users to schedule their days by saving events for each hour in a typical workday. Saved events are available to users even after closing or refreshing the browser.",
+        "This daily planner allows users to schedule their days by saving events for each hour in a typical workday. Saved events are available to users even after closing or refreshing the browser. Built with JavaScript, HTML, and CSS.",
       deployed: "https://beatricekallen.github.io/work-day-scheduler/",
       github: "https://github.com/beatricekallen/work-day-scheduler",
     },
